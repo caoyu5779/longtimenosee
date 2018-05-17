1b79351a75322210947036dac19611d8a818b5ba
pengcheng8 <pengcheng8@staff.weibo.com>
2018-05-17 19:14:50 +0800

    roman

---
+++ src/Leetcode/LongestCommon.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func LongestCommon(strs []string) string {
+	short := shortest(strs)
+
+	for i,r := range short{
+		for j := 0;j<len(strs); j++{
+			if strs[j][i] != byte(r){
+				return strs[j][:i]
+			}
+		}
+	}
+
+	return short
+}
+
+func shortest(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	res := strs[0]
+
+	for _,s := range strs {
+		if len(res) > len(s){
+			res = s
+		}
+	}
+
+	return res
+}
+
+func main(){
+	strs := []string{"flower","flow","flight"}
+	res := LongestCommon(strs)
+
+	fmt.Println(res)
+}
